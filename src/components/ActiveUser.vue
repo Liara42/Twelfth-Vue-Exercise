bd79ab1fe27414ee6cba65fc4e221e6a21bc7545
Liara42 <caroline1481@gmail.com>
2020-10-27 22:36:42 +0100

    Initial commit

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <h2>Name: {{ name }}</h2>
+    <h3>Age: {{ age }}</h3>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "age"],
+  //   data() {
+  //     return {};
+  //   },
+  //   methods: {},
+};
+</script>
